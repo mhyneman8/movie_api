b813a6d2c1f6022516b35ad3dccd07657d808a95
mhyneman8 <mallory.hyneman@gmail.com>
2021-05-12 14:15:02 -0500

    2.9 added authentication/ authorization

--- index.js
+++ index.js
@@ -8,95 +8,17 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+
+
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+let auth = require('./auth')(app);
 
-let movie = [
-    {
-        title: 'Cool Runnings',
-        director: 'Jon Turteltaub',
-        cast: ['John Candy', 'Leon', 'Doug E. Doug'],
-        genre: ['Adventure', 'Comedy', 'Family'],
-        description: 'When a Jamaican sprinter is disqualified from the Olympic Games, he enlists the help of a dishonored coach to start the first Jamaican Bobsled Team.',
-        image: 'https://m.media-amazon.com/images/M/MV5BMjMxMTQ3MzMwMV5BMl5BanBnXkFtZTgwNTYxNzYxMTE@._V1_UX182_CR0,0,182,268_AL_.jpg'
+const passport = require('passport');
+require('./passport');
 
-    },
-    {
-        title: 'Boyhood',
-        director: 'RichardLinklater',
-        cast: ['Ellar Coltrane', 'Patricia Arquette', 'Ethan Hawke'],
-        genre: 'Drama',
-        description: 'The life of Mason, from early childhood to his arrival at college.',
-        image: 'https://m.media-amazon.com/images/M/MV5BMTYzNDc2MDc0N15BMl5BanBnXkFtZTgwOTcwMDQ5MTE@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'Seven',
-        director: 'David Fincher',
-        cast: ['Morgan Freeman', 'Brad Pitt', 'Kevin Spacey'],
-        genre: ['Crime', 'Drama', 'Mystery'],
-        description: 'Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.',
-        image: 'https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'Casablanca',
-        director: 'Michael Curtiz',
-        cast: ['Humphrey Bogart', 'Ingrid Bergman', 'Paul Henreid'],
-        genre: ['Drama', 'Romance', 'War'],
-        description: 'A cynical expatriate American cafe owner struggles to decide whether or not to help his former lover and her fugitive husband escape the Nazis in French Morocco.',
-        image: 'https://m.media-amazon.com/images/M/MV5BY2IzZGY2YmEtYzljNS00NTM5LTgwMzUtMzM1NjQ4NGI0OTk0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'Across the Universe',
-        director: 'Julie Taymor',
-        cast: ['Evan Rachel Wood', 'Jim Sturgess', 'Joe Anderson'],
-        genre: ['Drama', 'Fantasy', 'History'],
-        description: 'The music of The Beatles and the Vietnam War form the backdrop for the romance between an upper-class American girl and a poor Liverpudlian artist.',
-        image: 'https://m.media-amazon.com/images/M/MV5BMTIyMTUwNzg3Nl5BMl5BanBnXkFtZTcwMjM1MDI1MQ@@._V1_UX182_CR0,0,182,268_AL_.jpg'
-
-    },
-    {
-        title: 'The Good, The Bad and the Ugly',
-        director: 'Sergio Leone',
-        cast: ['Clint Eastwood', 'Eli Wallach', 'Lee Van Cleef'],
-        genre: 'Western',
-        description: 'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.',
-        image: 'https://m.media-amazon.com/images/M/MV5BOTQ5NDI3MTI4MF5BMl5BanBnXkFtZTgwNDQ4ODE5MDE@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'The Shawshank Redemption',
-        director: 'Frank Darabont',
-        cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
-        genre: 'Drama',
-        description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-        image: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'Arsenic and Old Lace',
-        director: 'Frank Capra',
-        cast: ['Cary Grant', 'Priscilla Lane', 'Raymond Massey'],
-        genre: ['Comedy', 'Crime', 'Thriller'],
-        description: 'A writer of books on the futility of marriage risks his reputation when he decides to get married. Things get even more complicated when he learns on his wedding day that his beloved maiden aunts are habitual murderers.',
-        image: 'https://m.media-amazon.com/images/M/MV5BZDVlNTBjMjctNjAzNS00ZGJhLTg2NzMtNzIwYTIzYTBiMDkyXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'Almost Famous',
-        director: 'Cameron Crowe',
-        cast: ['Billy Crudup', 'Patrick Fugit', 'Kate Hudson'],
-        genre: ['Adventure', 'Comedy', 'Drama'],
-        description: 'A high-school boy is given the chance to write a story for Rolling Stone Magazine about an up-and-coming rock band as he accompanies them on their concert tour.',
-        image: 'https://m.media-amazon.com/images/M/MV5BMzY1ZjMwMGEtYTY1ZS00ZDllLTk0ZmUtYzA3ZTA4NmYwNGNkXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    },
-    {
-        title: 'That Thing You Do',
-        director: 'Tom Hanks',
-        cast: ['Tom Hanks', 'Liv Tyler', 'Carleze Theron'],
-        genre: ['Comedy', 'Drama', 'Music'],
-        description: 'A local Pennsylvania band scores a one-hit wonder in 1964 and rides the star-making machinery as long as they can, with lots of help from their manager.',
-        image: 'https://m.media-amazon.com/images/M/MV5BOWVmN2ZhZjgtZGEzMy00NDkxLWI5YWQtYTE2ZTk0YzIyMzc0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg'
-    }
-];
 
 app.use(morgan('common'));
 app.use(express.static('public'));
@@ -108,15 +30,15 @@ app.use((err, req, res, next) => {
 
 // GET requests
 app.get('/', (req, res) => {
-    res.send('Here are my top movies!');
+    res.send('Welcome to myFlix! Where you can keep track of all your favorite movies.');
 });
 
-app.get('/documentation', (req, res) => {
+app.get('/documentation', passport.authenticate('jwt', { session: false }), (req, res) => {
     res.sendFile('public/documentation.html', {root: __dirname});
 });
 
 // Gets the list of data about all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
@@ -128,7 +50,7 @@ app.get('/movies', (req, res) => {
 });
 
 // Return data about single movie
-app.get('/movies/:title', (req, res) => {
+app.get('/movies/:title', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.findOne({ 'Title': req.params.title})
     .then((movies) => {
         res.status(201).json(movies);
@@ -140,7 +62,7 @@ app.get('/movies/:title', (req, res) => {
 });
 
 // Return data about a genre by name
-app.get('/movies/genre/:Name', (req, res) => {
+app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.findOne({ 'Genre.Name': req.params.Name})
     .then((movies) => {
         res.json(movies.Genre);
@@ -152,7 +74,7 @@ app.get('/movies/genre/:Name', (req, res) => {
 });
 
 // Returns data about a director by name
-app.get('/movies/director/:Name', (req, res) => {
+app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.findOne({ 'Director.Name': req.params.Name })
     .then((movies) => {
         res.json(movies.Director);
@@ -198,7 +120,7 @@ app.post('/users', (req, res) => {
 });
 
 // Gets all users
-app.get('/users', (req, res) => {
+app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.find()
         .then((users) => {
             res.status(201).json(users);
@@ -210,7 +132,7 @@ app.get('/users', (req, res) => {
 });
 
 // Get a user by username
-app.get('/users/:Username', (req, res) => {
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOne({ Username: req.params.Username })
         .then((user) => {
             res.json(user);
@@ -222,7 +144,7 @@ app.get('/users/:Username', (req, res) => {
 });
 
 // Allows user to update their user info
-app.put('/users/:Username', (req, res) => {
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, 
         { $set: 
         {
@@ -244,7 +166,7 @@ app.put('/users/:Username', (req, res) => {
 });
 
 // Allows users to add movie to their list of favorites
-app.post('/users/:Username/Movies/:MovieID', (req, res) => {
+app.post('/users/:Username/Movies/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
        Users.findOneAndUpdate({ Username: req.params.Username }, {
            $push: { FavoriteMovies: req.params.MovieID }
        },
@@ -260,7 +182,7 @@ app.post('/users/:Username/Movies/:MovieID', (req, res) => {
     });
 
 // Removes movie from favorite list
-app.post('/users/:Username/Movies/remove/:MovieID', (req, res) => {
+app.post('/users/:Username/Movies/remove/:MovieID', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.Username }, {
         $pull: { FavoriteMovies: req.params.MovieID }
     },
@@ -276,7 +198,7 @@ app.post('/users/:Username/Movies/remove/:MovieID', (req, res) => {
 });
 
 // Allow users to deregister
-app.delete('/users/:Username', (req, res) => {
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
     Users.findOneAndRemove({ Username: req.params.Username })
     .then((user) => {
         if (!user) {
