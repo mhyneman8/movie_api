fdce8b93acfdb2963b22d999009a043df28c2c51
mhyneman8 <mallory.hyneman@gmail.com>
2021-05-15 10:19:49 -0500

    fix typo

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false 
 */
 app.post('/users',
     [
-        check('Username', 'Username is required').isLength({mind: 5}),
+        check('Username', 'Username is required').isLength({min: 5}),
         check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
         check('Password', 'Password is required').not().isEmpty(),
         check('Email', 'Email does not appear to be valid').isEmail()
