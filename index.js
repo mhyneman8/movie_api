9548925e7d233b93589a62f4012f7b260527f25f
mhyneman8 <mallory.hyneman@gmail.com>
2021-05-27 09:12:13 -0500

    cors issues

--- index.js
+++ index.js
@@ -23,16 +23,17 @@ const cors = require('cors');
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
-app.use(cors({
-    origin: (origin, callback) => {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            let message = "The CORS policy for this application doesn't allow acces from origin " + origin;
-            return callback(new Error(message), false);
-        }
-        return callback(null, true);
-    }
-}));
+// change back to use(cors({
+app.use(cors());
+//     origin: (origin, callback) => {
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             let message = "The CORS policy for this application doesn't allow acces from origin " + origin;
+//             return callback(new Error(message), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 let auth = require('./auth')(app);
 
